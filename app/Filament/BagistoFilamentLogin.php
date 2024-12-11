cea1b550a731dc8f94116b4ba455d6913103c25a
Teranveril <sonofselorin@gmail.com>
2024-12-11 14:38:18 +0100

    start2

---
+++ app/Filament/BagistoFilamentLogin.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Filament;
+
+use Filament\Forms\Form;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Component;
+use Filament\Pages\Auth\Login as BaseAuth;
+
+class BagistoFilamentLogin extends BaseAuth
+{
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                $this->getEmailFormComponent(),
+                $this->getPasswordFormComponent(),
+                $this->getRememberFormComponent(),
+            ])
+            ->statePath('data');
+    }
+
+    protected function getLoginFormComponent(): Component
+    {
+        return TextInput::make('login')
+            ->label('Login')
+            ->required()
+            ->autocomplete()
+            ->autofocus()
+            ->extraInputAttributes(['tabindex' => 1]);
+    }
+}
